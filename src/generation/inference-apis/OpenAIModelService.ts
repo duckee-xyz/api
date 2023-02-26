71f462a507c5046d2952d32722369d351486d24a
therne <retail3210@gmail.com>
2023-02-27 00:59:35 +0900

    fix: Persist OpenAI generation result to S3

--- src/generation/inference-apis/OpenAIModelService.ts
+++ src/generation/inference-apis/OpenAIModelService.ts
@@ -1,8 +1,10 @@
+import axios from 'axios';
 import { randomUUID } from 'crypto';
 import { Configuration, OpenAIApi } from 'openai';
+import { log } from 'pine-log';
 import { Service } from 'typedi';
 import { Repository } from 'typeorm';
-import { InjectRepository } from '../../utils';
+import { InjectRepository, uploadToS3 } from '../../utils';
 import { OpenAIGenerationStatus } from '../entities';
 import { GenerationConfig } from '../GenerationConfig';
 import { GenerateTaskStatus } from '../types';
@@ -32,12 +34,25 @@ export class OpenAIModelService implements IModelService {
         response_format: 'url',
         n: 1,
       })
-      .then(({ data }) => {
+      .then(async ({ data }) => {
+        const resultUrl = data.data[0].url;
+        log.trace('OpenAI image generation completed', { id: taskStatus.id, resultUrl });
+
+        const uploadImageToS3 = await uploadToS3(`/images/${taskStatus.id}.png`);
+
+        const downloadImageFromOpenAI = await axios({
+          method: 'GET',
+          url: resultUrl,
+          responseType: 'stream',
+        });
+        downloadImageFromOpenAI.data.pipe(uploadImageToS3.stream);
+        await uploadImageToS3.done;
+
         return this.generationStatusRepository.update(
           { id: taskStatus.id },
           {
             status: 'completed',
-            resultImageUrl: data.data[0].url,
+            resultImageUrl: uploadImageToS3.url,
           },
         );
       })
